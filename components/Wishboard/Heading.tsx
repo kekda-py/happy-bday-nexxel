a855505892a281bf9f75e8780e86661318268ab4
Yash <93475253+kekda-py@users.noreply.github.com>
2022-06-16 18:42:30 +0530

    Update Heading.tsx

--- components/Wishboard/Heading.tsx
+++ components/Wishboard/Heading.tsx
@@ -1,7 +1,7 @@
 const Heading: React.FC = () => {
   return (
     <h1 className="absolute px-4 text-4xl text-center text-white capitalize top-6 font-roboto">
-      wishes for <span className="text-pink-300">rachey</span>
+      wishes for <span className="text-pink-300">nexxel</span>
     </h1>
   );
 };
